36c37471c773b1749b1271b179326641efc26d66
igor <ighor.naumov.2002mail@mail.ru>
2021-12-09 01:14:39 +0500

    add utils.py

---
+++ modules/utils.py
@@ -0,0 +1,22 @@
+import sys
+
+import requests
+
+answer_dict = {'yes': True, 'y': True, 'no': False, 'n': False}
+
+
+def get_msg_if_response_not_ok(response: requests.Response) -> str:
+    try:
+        response.raise_for_status()
+    except Exception as e:
+        return str(e)
+
+
+def get_answer_yes_or_no() -> bool:
+    try:
+        answer = answer_dict[input().lower()]
+    except KeyError:
+        print('Abort.')
+        sys.exit()
+
+    return answer
