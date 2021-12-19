a1a6b893f09d93570de1bd10a8596fcc21b633f5
igor <ighor.naumov.2002mail@mail.ru>
2021-12-19 16:22:08 +0500

    update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,13 @@
 import json
 import logging.config
 from os import path, remove
+from pathlib import Path
+
+from modules.infrastructure.file_manager import FileManager
+
+
+FileManager.make_directory_if_not_exist(Path(__file__).resolve().parent.parent, 'Logs')
+
 
 if path.isfile("Logs/logger.log"):
     remove("Logs/logger.log")
